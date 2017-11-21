f043f4401254a9389758214c54874e485de1fa42
Brendan Wascher <Brendan.Wascher@iam.local>
2017-11-20 18:07:16 -0600

    Other Changes

---
+++ Assets/Scripts/InteractiveObjectBasic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveObjectBasic : MonoBehaviour, IActivatable
+{
+
+    public void DoActivate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
